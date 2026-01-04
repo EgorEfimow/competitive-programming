d39ad62e34d7e677b40c02b88f95040e0b43c789
EgorEfimow <eaefimov@proton.me>
2026-01-04 16:40:19 +0300

    feat (atcoder): add solution for ABC434_A

---
+++ AtCoder/abc434a/abc434a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define en '\n'
+
+void solve() {
+    int w, b;
+    cin >> w >> b;
+
+    cout << 1000*w / b + 1 << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
