696206bfb5f10c68a09d4cab72332c5491e0a229
Egor Efimov <eaefimov@proton.me>
2026-03-26 17:22:06 +0300

    feat (atcoder): add solution for abc122b (easy level bootcamp)

---
+++ AtCoder/abc122b/abc122b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    string s;
+    cin >> s;
+
+    int ans = 0, curlen = 0;
+    for (int i = 0; i < isz(s); ++i) {
+        if (s[i] == 'A' || s[i] == 'G' || s[i] == 'C' || s[i] == 'T') {
+            curlen++;
+            ans = max(curlen, ans);
+        } 
+        else curlen = 0;
+    }
+    cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
