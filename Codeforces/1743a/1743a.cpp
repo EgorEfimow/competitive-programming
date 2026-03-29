5f020d652a51bb47df670ae5577c9cb867888816
Egor Efimov <eaefimov@proton.me>
2026-03-29 13:58:02 +0300

    feat (codeforces): add solution for 1743a

---
+++ Codeforces/1743a/1743a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    int tmp;
+    for (int i = 0; i < n; ++i) cin >> tmp;
+
+    cout << (10 - n) * (9 - n) * 3 << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
