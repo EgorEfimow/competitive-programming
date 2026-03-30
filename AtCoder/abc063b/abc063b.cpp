71c78af6ca0405731408416d4b1f71a885e50947
Egor Efimov <eaefimov@proton.me>
2026-03-30 17:09:04 +0300

    feat (atcoder): add solution for abc063b (easy level bootcamp)

---
+++ AtCoder/abc063b/abc063b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    string s;
+    cin >> s;
+    set<char> st(all(s));
+
+    cout << (isz(st) == isz(s) ? "yes" : "no") << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
