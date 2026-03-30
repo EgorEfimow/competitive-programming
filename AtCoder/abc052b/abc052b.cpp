64eb687f25daed6dd2db2da23eb5131c99e8f155
Egor Efimov <eaefimov@proton.me>
2026-03-30 17:27:35 +0300

    feat (atcoder): add solution for abc052b (easy level bootcamp)

---
+++ AtCoder/abc052b/abc052b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int ans = 0, v = 0;
+    for (char &e : s) {
+        if (e == 'I') v++;
+        else v--;
+        ans = max(ans, v);
+    }
+    cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
