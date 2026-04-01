e8218c551ebfa6a5e23585d3c48f721764953c4f
Egor Efimov <eaefimov@proton.me>
2026-04-01 22:40:05 +0300

    feat (codeforces): add solution for 1987a

---
+++ Codeforces/1987a/1987a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    cout << (n - 1) * k + 1 << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
