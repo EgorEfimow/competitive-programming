cc2ccbd0623b877a49923342a4143fa60e11fd45
Egor Efimov <eaefimov@proton.me>
2026-04-02 13:05:46 +0300

    feat (atcoder): add solution for AGC024A (easy level bootcamp)

---
+++ AtCoder/agc024a/agc024a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    ll a, b, c, k; 
+    cin >> a >> b >> c >> k;
+
+    cout << ((k & 1) ? b - a : a - b) << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
