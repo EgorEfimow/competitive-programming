3012aff7e48e75358d14152c69b803564c67dd64
Egor Efimov <eaefimov@proton.me>
2026-04-05 22:01:25 +0300

    feat (codeforces): add solution for 2176a

---
+++ Codeforces/2176a/2176a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    int n; cin >> n;
+    
+    int ans = 0, sv;
+    cin >> sv;
+    for (int i = 1; i < n; ++i) {
+        int t; cin >> t;
+        if (t < sv) ans++;
+        else sv = t;
+    }
+    cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
