8f2a7ce4ca975850390f77f2af88633ff3a1d637
Egor Efimov <eaefimov@proton.me>
2026-04-05 22:28:46 +0300

    feat (codeforces): add solution for 1944a

---
+++ Codeforces/1944a/1944a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    int n, k; cin >> n >> k;
+    if (k >= n - 1) cout << 1 << en;
+    else cout << n << en;    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
