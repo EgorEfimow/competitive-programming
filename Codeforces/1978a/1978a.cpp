99e6897e442212e91c36f0a47125f9fceb084a8b
Egor Efimov <eaefimov@proton.me>
2026-04-05 22:11:47 +0300

    feat (codeforces): add solution for 1978a

---
+++ Codeforces/1978a/1978a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    int n; cin >> n;
+    vector<int> v(n);
+    for (int &e : v) cin >> e;
+
+    int m = *max_element(v.begin(), v.end() - 1);
+
+    cout << m + v.back() << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
