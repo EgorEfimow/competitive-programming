92269bde01284a85e97e95c41623c83be9d85b4c
Egor Efimov <eaefimov@proton.me>
2026-04-07 08:14:59 +0300

    feat (atcoder): add solution for abc053b (easy level bootcamp)

---
+++ AtCoder/abc053b/abc053b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+	string s; cin >> s;
+	
+	cout << abs(distance(s.rend(), find(rall(s), 'Z'))) - distance(s.begin(), find(all(s), 'A')) << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
