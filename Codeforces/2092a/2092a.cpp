5dfedaefcc8713dcfcf81bcc7ceeaaeb1b3711a4
Egor Efimov <eaefimov@proton.me>
2026-04-07 23:20:56 +0300

    feat (codeforces): add solution for 2092a

---
+++ Codeforces/2092a/2092a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    int n; cin >> n;
+    vector<int> v(n);
+
+    for (int &e : v) cin >> e;
+
+    sort(all(v));
+
+    int mx = v.back(), l = v[0];
+
+    cout << mx - l  << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
