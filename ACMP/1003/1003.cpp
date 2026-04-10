d10bccfda6ade3ed5761d3bb5d4887f4966c24d9
Egor Efimov <eaefimov@proton.me>
2026-04-10 22:38:43 +0300

    feat (acmp): add solutions for formulas block

---
+++ ACMP/1003/1003.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    ll x, y, z; cin >> x >> y >> z;
+    cout << 3 * x + 5 * y + 12 * z << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ ACMP/92/92.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    int s; cin >> s;
+    cout << s / 6 << " " << (s * 4) / 6 << " " << s / 6 << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
