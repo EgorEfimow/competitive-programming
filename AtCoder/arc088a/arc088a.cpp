9af6dae32765fbc51a6291ca847ea7965ad96b3b
Egor Efimov <eaefimov@proton.me>
2026-04-11 16:59:50 +0300

    feat (atcoder): add solution for arc088a (easy level bootcamp)

---
+++ AtCoder/arc088a/arc088a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+#define ll long long 
+
+void solve() {
+    ll x, y; cin >> x >> y;
+
+    ll ans = 0;
+
+    for (ll i = x; i <= y; i *= 2) ans++;
+    cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
