1ff7abb99d81ddad47c2bc07466716fc285f3717
Egor Efimov <eaefimov@proton.me>
2026-04-13 12:13:16 +0300

    feat (atcoder): add solution for abc046b (easy level bootcamp)

---
+++ AtCoder/abc046b/abc046b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vii vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    ll a, b; cin >> a >> b;
+    cout << 1ll*b * ((ll)pow(b - 1, a - 1)) << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
