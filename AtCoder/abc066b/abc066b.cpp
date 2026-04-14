adaac15f9e3b9937ec81849fca6ef6650cb17889
Egor Efimov <eaefimov@proton.me>
2026-04-14 18:12:40 +0300

    feat (atcoder): add solution for abc066b (medium level bootcamp)

---
+++ AtCoder/abc066b/abc066b.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vii vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    string s; cin >> s;
+
+    int n = isz(s);
+    for (int i = 0; i < n; ++i) {
+        s.pop_back();
+        if (isz(s) % 2) continue;
+        string q = s.substr(0, isz(s) / 2), w = s.substr(isz(s) / 2);
+
+        if (q == w) {
+            cout << isz(q) * 2 << en;
+            return;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
