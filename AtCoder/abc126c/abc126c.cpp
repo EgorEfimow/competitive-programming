fa50c7fe0fbcb972a7b0433f8eb01b4b7d187fb3
Egor Efimov <eaefimov@proton.me>
2026-04-18 12:21:48 +0300

    feat (atcoder): add solution for abc126c (medium level bootcamp)

---
+++ AtCoder/abc126c/abc126c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    int n, k; cin >> n >> k;
+
+    double ans = 0, p = 1.0 / n;
+    for (int i = 1; i <= n; ++i) {
+        int cnt = 0;
+        ll d = i;
+        while (d < k) {
+            cnt++;
+            d *= 2;
+        }
+        ans += p * pow(0.5, cnt); 
+
+    }
+    cout << fixed << setprecision(15) << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
