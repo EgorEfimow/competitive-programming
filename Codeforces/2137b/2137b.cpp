9b41d5390f5404873ef8cf688ca905ca700f08f0
Egor Efimov <eaefimov@proton.me>
2026-04-18 17:10:46 +0300

    feat (codeforces): add solution for 2137b

---
+++ Codeforces/2137b/2137b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int v; cin >> v;
+        cout << n - v + 1 << " ";
+    }
+    cout << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
