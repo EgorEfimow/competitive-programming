123ce99e3f98c26998e23c1e2001dd14d204f173
Egor Efimov <eaefimov@proton.me>
2026-04-19 18:04:30 +0300

    feat (codeforces): add solution for 1945b

---
+++ Codeforces/1945b/1945b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    ll a, b, m;
+    cin >> a >> b >> m;
+
+    cout << 2 + (m / a) + (m / b) << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
