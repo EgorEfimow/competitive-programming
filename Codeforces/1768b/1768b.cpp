920b186e7b0ba65769b514d1a475eb3264289af1
Egor Efimov <eaefimov@proton.me>
2026-04-20 17:34:08 +0300

    feat (codeforces): add solution for 1768b

---
+++ Codeforces/1768b/1768b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vi v(n);
+    for (int &e : v) cin >> e;
+
+    int len = 0, i = 0;
+    while (i < n) {
+        if (v[i] - 1 == len)
+            len++;
+        i++;
+    }
+
+    cout << (n - len + (k - 1)) / k << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
