80396d29bd1e7a6158155d844fcca2bd9077f7ce
Egor Efimov <eaefimov@proton.me>
2026-05-02 10:26:47 +0500

    feat (codeforces): add solution for 1295a

---
+++ Codeforces/1295a/1295a.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    int n; cin >> n;
+    if (n % 2) {
+        cout << 7;
+        for (int i = 0; i < n - 3; i += 2) cout << 1;
+        cout << en;
+        return;
+    }
+    
+    cout << string(n / 2, '1') << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
