ccd1a6c7437192888ec539bc56c4c206c8d2ff9c
Egor Efimov <eaefimov@proton.me>
2026-05-03 13:35:56 +0500

    feat (codeforces): add solution for 2062b

---
+++ Codeforces/2062b/2062b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    ll n; cin >> n;
+    vll v(n);
+    for (ll &e : v) cin >> e;
+
+    for (ll i = 0; i < n / 2 + 1; ++i) {
+        ll d = 2 * (n - i - 1);
+        if (v[i] <= d || v[n - i - 1] <= d) {
+            cout << "no" << en;
+            return;
+        }
+    }
+    cout << "yes" << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
