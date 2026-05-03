d3397899a4ca7378fa1c0377e6f71d7bdce88cf0
Egor Efimov <eaefimov@proton.me>
2026-05-04 01:06:14 +0500

    feat (acmp): add solutions

---
+++ ACMP/62/62.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    string s;
+    cin >> s;
+    if ((s[0] - 'A' + 1) % 2 == (s[1] - '0') % 2)
+        cout << "BLACK" << en;
+    else
+        cout << "WHITE" << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
