8c56f6ea36a92eeed64cae944cc8cb4daf54bbc2
Egor Efimov <eaefimov@proton.me>
2026-05-15 16:28:46 +0500

    feat (atcoder): add solution for arc131a

---
+++ AtCoder/arc131a/arc131a.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    ll a, b; cin >> a >> b;
+
+    string res = "";
+    res += to_string(a);
+    string bb = to_string(b / 2);
+    if (b % 2 == 0)
+        res += to_string(b / 2);
+    else 
+        res += bb + "5";
+
+    cout << res << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
