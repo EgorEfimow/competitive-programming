1b6819307f48de9b62f4358c6123d6907ce7bbec
Egor Efimov <eaefimov@proton.me>
2026-05-15 16:37:59 +0500

    feat (atcoder): add solution for codefestival2015qualbA

---
+++ AtCoder/codefestival_2015_qualB_a/codefestival_2015_qualB_a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    string s; cin >> s;
+    map<char, int> mp;
+    for (char &e : s) mp[e] += 2;
+
+    for (auto &[k, v] : mp) {
+        cout << string (v, k);
+    }
+    cout << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
