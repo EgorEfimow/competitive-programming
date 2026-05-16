ef043dbff093901e7155fbbae34572845bf5be56
Egor Efimov <eaefimov@proton.me>
2026-05-16 22:38:17 +0500

    feat (atcoder): add solution for abc330b

---
+++ AtCoder/abc330b/abc330b.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    int n, l, r; cin >> n >> l >> r;
+    
+    for (int i = 0; i < n; ++i) {
+        int q; cin >> q;
+        if (q <= l) cout << l << " ";
+        else if (q >= r) cout << r << " ";
+        else cout << q << " ";
+    }
+    cout << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
