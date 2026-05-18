c560db81bd100d71dc4fe83ae2c01bb664ffe651
Egor Efimov <eaefimov@proton.me>
2026-05-18 17:02:20 +0500

    feat (codeforces): add solution for 1165b

---
+++ Codeforces/1165b/1165b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    int n; cin >> n;
+    multiset<int> mst;
+    for (int i = 0; i < n; ++i) {
+        int q; cin >> q;
+        mst.insert(q);
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        auto it = mst.lower_bound(i);
+        if (it == mst.end()) break;
+        mst.erase(it);
+        ans++;
+    }
+    cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
