475acdb9b5ae7f25271844d1c497f0a594237fd2
Egor Efimov <eaefimov@proton.me>
2026-05-18 15:24:09 +0500

    feat (codeforces): add solution for 1837c

---
+++ Codeforces/1837c/1837c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    string s; cin >> s;
+    char lastSeen = '0';
+    for (char &c : s) {
+        if (c == '?') continue;
+        lastSeen = c;
+        break;
+    }
+    for (int i = 0; i < isz(s); ++i) {
+        if (s[i] == '?') s[i] = lastSeen;
+        else lastSeen = s[i];
+    }
+    cout << s << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
