7efd2af4602e2e78f7fdc4f018691255485f9dcd
Egor Efimov <eaefimov@proton.me>
2026-06-02 12:20:54 +0500

    feat (codeforces): add solution for 1861b

---
+++ Codeforces/1861b/1861b.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    string a; cin >> a;
+    string b; cin >> b;
+
+    for (int i = 0; i < isz(a) - 1; ++i) {
+        if ((a[i] == b[i] && a[i] == '0') && (a[i + 1] == b[i + 1] && a[i + 1] == '1')) {
+            cout << "yes" << en;
+            return;
+        }
+    }
+    cout << "no" << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
