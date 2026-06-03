ddffb53f27df1600ee183cef979343602df7db0e
Egor Efimov <eaefimov@proton.me>
2026-06-03 13:02:58 +0500

    feat (codeforces): add solution for 1766b

---
+++ Codeforces/1766b/1766b.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define en '\n'
+#define ll long long 
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int, int>
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+
+    for (int i = 0; i < n - 2; ++i) {
+        if (s.find(s.substr(i, 2), i + 2) != string::npos) {
+            cout << "YES" << en;
+            return;
+        }
+    }
+    cout << "NO" << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
