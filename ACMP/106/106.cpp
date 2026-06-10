7f6094d21a8ef230c29f9151c4e145fe4c4bee6d
Egor Efimov <eaefimov@proton.me>
2026-06-10 16:10:30 +0500

    feat (acmp): add solutions

---
+++ ACMP/106/106.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#pragma region TEMPLATES
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+#pragma endregion
+
+void solve() {
+    int n; cin >> n;
+    vi v(n); cin >> v;
+
+    int cnt0 = 0, cnt1 = 0;
+    for (int &e : v) {
+        if (e == 1) cnt1++;
+        else cnt0++;
+    }
+    cout << n - max(cnt0, cnt1) << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
