6befac8d41df44cfc83c48143858a539b413336b
Egor Efimov <eaefimov@proton.me>
2026-06-10 20:01:45 +0500

    feat (acmp): add solutions

---
+++ ACMP/233/233.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#pragma region TEMPLATES
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+#pragma endregion
+
+void solve() {
+    int n; cin >> n;
+    vi v(n); cin >> v;
+
+    int h = 437;
+
+    for (int i = 0; i < n; ++i) {
+        if (v[i] > h) continue;
+        cout << "Crash " << i + 1 << en;
+        return;
+    }
+    cout << "No crash" << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
