cc884f5b5441a80caf684cad6ecc327690ffdb90
Egor Efimov <eaefimov@proton.me>
2026-06-13 19:00:27 +0500

    feat (atcoder): add solution for abc369c

---
+++ AtCoder/abc369c/abc369c.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+void solve() {
+    ll n; cin >> n;
+    vll v(n); cin >> v;
+
+    vll diff(n - 1);
+    for (int i = 0; i < n - 1; ++i) {
+        diff[i] = v[i + 1] - v[i];
+    }
+
+    ll ans = 0, r = 0;
+    bool first = true;
+    while (r < isz(diff)) {
+        ll ptr = r, len = 0;
+        while (ptr < isz(diff) && diff[r] == diff[ptr]) {
+            ptr++;
+            len++;
+        }
+        
+        len++;
+        ans += (len * (len + 1ll)) / 2ll;
+
+        if (!first) ans--;
+        else first = false;
+
+        r = ptr;
+    }
+
+    cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
