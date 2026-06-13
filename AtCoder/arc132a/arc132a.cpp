8947ed04ad4941301ce610c4349f965993ee1551
Egor Efimov <eaefimov@proton.me>
2026-06-13 21:06:51 +0500

    feat (atcoder): add solution for arc132a

---
+++ AtCoder/arc132a/arc132a.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+void solve() {
+    int n; cin >> n;
+    vi r(n); cin >> r;
+    vi c(n); cin >> c;
+
+    int q; cin >> q;
+    for (int i = 0; i < q; ++i) {
+        int a, b; cin >> a >> b;
+        a--; b--;
+        if (r[a] + c[b] < n + 1) cout << '.';
+        else cout << "#";
+    }
+    cout << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
