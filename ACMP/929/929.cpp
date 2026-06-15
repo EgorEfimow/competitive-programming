90e11cfc328c21f076abcc1916b1d8ff73ccd4b5
Egor Efimov <eaefimov@proton.me>
2026-06-15 10:52:57 +0500

    feat (acmp): add solutions

---
+++ ACMP/929/929.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+void solve() {
+    ll n; cin >> n;
+
+    vll arr = {0, 6, 5, 4, 3, 2, 1};
+
+    ll ma = n * 6;
+    ll mi = 0;
+
+    ll cnt = n / 6;
+    mi += cnt + arr[n - cnt * 6];
+
+    cout << mi << " " << ma << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
