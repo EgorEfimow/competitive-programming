a466ab31045035604b33f9864feccafea5290590
Egor Efimov <eaefimov@proton.me>
2026-06-21 20:44:11 +0500

    feat (codeforces): add solution for 2131c

---
+++ Codeforces/2131c/2131c.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+ll binpow(ll a, ll b, ll MOD) {
+    a %= MOD;
+    ll res = 1;
+    while (b) {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vi s(n), t(n); cin >> s >> t;
+
+    for (int i = 0; i < n; ++i) {
+        s[i] = min(s[i] % k, k - s[i] % k);
+        t[i] = min(t[i] % k, k - t[i] % k);
+    }
+
+    sort(all(s)); sort(all(t));
+    if (s != t) cout << "no" << en;
+    else cout << "yes" << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
