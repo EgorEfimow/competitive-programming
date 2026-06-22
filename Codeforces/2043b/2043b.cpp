384c8aee923c17ce3261fb9deb13cc7964e46174
Egor Efimov <eaefimov@proton.me>
2026-06-22 18:23:53 +0500

    feat (codeforces): add solution for 2043b

---
+++ Codeforces/2043b/2043b.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+ll binpow(ll a, ll b, ll MOD) {
+    a %= MOD;
+    ll res = 1;
+    while (b) {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n, d; cin >> n >> d;
+
+    cout << 1 << " ";
+
+    if (n > 2 || (d % 3 == 0)) cout << 3 << " ";
+
+    if (d == 5) cout << 5 << " ";
+
+    if (n > 2 || (d % 7 == 0)) cout << 7 << " ";
+
+    if (n > 5 || (d % 9 == 0) || (n > 2 && n < 6 && (d % 3 == 0 || d % 6 == 9)))
+        cout << 9;
+
+    cout << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
