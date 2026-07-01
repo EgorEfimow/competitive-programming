261f902d520744e32c71698057a21040cfa05620
Egor Efimov <eaefimov@proton.me>
2026-07-01 12:11:10 +0500

    feat (codeforces): add solution for 2162c

---
+++ Codeforces/2162c/2162c.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+ll binpow(ll a, ll b, ll MOD) {
+    a %= MOD;
+    ll res = 1;
+    while (b) {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int a, b; cin >> a >> b;
+    
+    if (a == b) {
+        cout << 0 << en;
+        return;
+    }
+    
+    if (__lg(a) < __lg(b)) {
+        cout << -1 << en;
+        return;
+    }
+
+    // check how value changes
+    int pw = 1, cnt = 0;
+    vi ans;
+    while(a) {
+        if ((b & 1) != (a & 1)) {
+            cnt++;
+            ans.push_back(pw);
+        }
+        pw *= 2;
+        a >>= 1;
+        b >>= 1;
+    }
+
+    cout << cnt << en;
+    cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
