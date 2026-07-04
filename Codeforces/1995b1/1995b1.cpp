24ab60e1496cf3f84c3edec0137a2fc8922f049a
Egor Efimov <eaefimov@proton.me>
2026-07-04 13:46:49 +0500

    feat (codeforces): add solution for 1995b1

---
+++ Codeforces/1995b1/1995b1.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+ll binpow(ll a, ll b, ll MOD) {
+    a %= MOD;
+    ll res = 1;
+    while (b) {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    ll n, m; cin >> n >> m;
+    vll v(n); cin >> v;
+
+    sort(all(v));
+
+    ll left = 0, sum = 0, ans = 0;
+    for (int right = 0; right < n; ++right) {
+        sum += v[right];
+
+        while (v[right] - v[left] > 1) {
+            sum -= v[left];
+            left++;
+        }
+
+        while (sum > m) {
+            sum -= v[left];
+            left++;
+        }
+
+        ans = max(sum, ans);
+    }
+
+    cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
