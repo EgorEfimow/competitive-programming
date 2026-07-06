2a14365124665235fc392f58db2529cfd8d913c7
Egor Efimov <eaefimov@proton.me>
2026-07-06 11:55:25 +0500

    feat (codeforces): add solution for 1979c

---
+++ Codeforces/1979c/1979c.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+ll binpow(ll a, ll b, ll MOD) {
+    a %= MOD;
+    ll res = 1;
+    while (b) {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n; cin >> n;
+    vi v(n); cin >> v;
+
+    ll val = 116396280;
+    ll mi = *min_element(all(v));
+
+    vll ans(all(v));
+
+    for (int i = 0; i < n; ++i) {
+        ans[i] = (mi * val) / ans[i];
+    }
+    ll sum = accumulate(all(ans), 0ll);
+    if (mi * val <= sum) cout << -1 << en;
+    else cout << ans << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
