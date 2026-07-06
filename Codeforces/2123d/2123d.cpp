a26ff2a5e7c622d4f2ce49afb6ae4eb27f12bda3
Egor Efimov <eaefimov@proton.me>
2026-07-06 13:11:06 +0500

    feat (codeforces): add solution for 2123d

---
+++ Codeforces/2123d/2123d.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+ll binpow(ll a, ll b, ll MOD) {
+    a %= MOD;
+    ll res = 1;
+    while (b) {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n, k; cin >> n >> k;
+    string s; cin >> s;
+
+    int cnt1 = 0;
+    for (char &c : s) {
+        if (c == '1') cnt1++;
+    }
+    if (k == 1 && cnt1 > 1) {
+        cout << "bob" << en;
+        return;
+    }
+    if (k >= cnt1) {
+        cout << "alice" << en;
+        return;
+    }
+    if (k > (n / 2))
+        cout << "alice" << en;
+    else cout << "bob" << en;
+    // count from
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
