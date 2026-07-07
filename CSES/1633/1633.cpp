8a39c82b24a267ed5a1dd5988598976af9442568
Egor Efimov <eaefimov@proton.me>
2026-07-07 14:57:51 +0500

    feat (cses): add solution (dp section)

---
+++ CSES/1633/1633.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define en '\n'
+
+using ll  = long long;
+using pii = pair<int, int>;
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) 
+        in >> i;
+    return in;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (const auto& i : a) { 
+        out << i << " ";
+    }
+    return out;
+}
+
+ll binpow(ll a, ll b, ll MOD) {
+    a %= MOD;
+    ll res = 1;
+    while (b) {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n; cin >> n;
+
+    const int sz = 1e6 + 1;
+    const int mod = 1e9 + 7;
+    int arr[sz];
+    fill(arr, arr + sz, 0);
+
+    for (int i = 0; i < 6; ++i) {
+        arr[i] = 1 << i;
+    }
+
+    for (int i = 6; i <= n; ++i) {
+        for (int j = 1; j <= 6; ++j) {
+            arr[i] = (arr[i] + arr[i - j]) % mod;
+        }
+    }
+    cout << arr[n - 1] << en;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
